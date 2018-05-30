8bd1ff0a55af9406beeef164c42fb6892e6a6364
yal2018 <lryal2012@gmail.com>
2018-05-30 18:52:22 -0300

    Changed the HTML for the site.

--- myvenv/blog/views.py
+++ myvenv/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request,
+                  'C:/Users/Luis/Python Dev/MotherSystem/djangogirls/myvenv/blog/templates/blog/post_list.html', {})
